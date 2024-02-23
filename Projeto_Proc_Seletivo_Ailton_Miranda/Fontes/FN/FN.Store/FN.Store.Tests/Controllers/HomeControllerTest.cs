b6d80fdebb137a47df7b9577152cd08e4fc6da81
AiltonMiranda <46692459+AiltonMiranda@users.noreply.github.com>
2024-02-23 11:59:35 -0300

    Add files via upload

---
+++ Projeto_Proc_Seletivo_Ailton_Miranda/Fontes/FN/FN.Store/FN.Store.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,35 @@
+﻿using FN.Store.UI.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace FN.Store.Tests.Controllers
+{
+    [TestCategory("Controllers")]
+    [TestClass]
+    public class HomeControllerTest
+    {
+        //dado um homecontroller
+
+        [TestMethod]
+        public void DeveraRetornarAViewIndex()
+        {
+            //arrange
+            var homeController = new HomeController();
+
+            //act
+            //var result = (ViewResult)homeController.Index();
+            //as = tenta converter, e se não conseguir, resulta em null
+            var result = homeController.Index() as ViewResult;
+
+            //assert
+            Assert.AreEqual(typeof(ViewResult), result.GetType());
+
+        }
+
+    }
+}
---
+++ Projeto_Proc_Seletivo_Ailton_Miranda/Fontes/FN/FN.Store/FN.Store.Tests/HelloWorldTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FN.Store.Tests
+{
+    [TestClass]
+    public class HelloWorldTest
+    {
+        [TestMethod]
+        public void MeuHelloWorldTest()
+        {
+            //a - arrange (ambiente)
+            int random = 0;
+
+            do
+            {
+                random = new Random().Next(10);
+            }
+            while (random % 2 != 0);
+
+
+            //a - act (ação do método)
+            var ehPar = random % 2 == 0;
+
+            //a - assert (verifica se deu tudo certo)
+            Assert.AreEqual(true, ehPar);
+        }
+
+
+    }
+}
